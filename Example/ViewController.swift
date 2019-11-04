b7ed72f8d25033c9a688aa562ba38ca2a7eb3dc1
Benjamin Dodgson <benji@tomorrow.me>
2019-11-03 16:45:15 -0800

    added example and futures class

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Benji Dodgson on 11/3/19.
+//  Copyright © 2019 Tomorrow Ideas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
